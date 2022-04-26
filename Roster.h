420eb87a1331071964dc737d046af8d1ba7bc504
hug0first <niallpepper@gmail.com>
2022-04-25 21:26:53 -0400

    added a map to Roster.h and some libraries

--- Roster.h
+++ Roster.h
@@ -1,6 +1,10 @@
 #ifndef _Roster_
 #define _Roster_
+#include <iostream>
+#include <map>
+#include "Player.h"
 
+using roster_map_ = std::map<std::string, Player>;
 class Roster //has the list of players and uses class Player to get info about each player
 {
 public:
@@ -8,7 +12,7 @@ public:
 	void run();//needs to be implemented, please update when implemented
 private:
 
-
+	roster_map_ roster;
 };
 
 #endif
