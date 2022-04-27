1b4976477213f9d7103173872776e936a7e65f9b
hug0first <niallpepper@gmail.com>
2022-04-26 21:05:50 -0400

    made make_player() function

--- Player.h
+++ Player.h
@@ -8,16 +8,19 @@
 class Player//contains the actual information of each player
 {
 public:
+	friend void Roster::make_player();
+
 	Player() : f_name_("Undefined"),l_name_("Undefined"), yob_(9999),reg_stat_(false) {}
-	Player(std::string fname, std::string lname, int yob, bool reg_stat) : 
+	Player(std::string fname, std::string lname, int yob, bool reg_stat,int season_year) : 
 		f_name_(fname), l_name_(lname), yob_(yob), reg_stat_(reg_stat) {
-		age_ = Roster::get_season_year() - yob_;
+		age_ = season_year - yob_;
 		category_ = calc_category();
 	}
 
 
 private:
 	std::string calc_category();
+	inline std::string get_l_name() { return l_name_; };
 
 	std::string f_name_;
 	std::string l_name_;
@@ -25,10 +28,16 @@ private:
 	bool reg_stat_;
 	int age_;
 	std::string category_;
+	std::string error_message = "Player too young or too old. ";//maybe expand on this
 };
 
 std::string Player::calc_category() {
-	
-	
+	if ((age_ < 17) && (age_ >= 14)) category_ = "U17";
+	if ((age_ < 14) && (age_ >= 12)) category_ = "U14";
+	if ((age_ < 12) && (age_ >= 10)) category_ = "U12";
+	if ((age_ < 10) && (age_ >= 8)) category_ = "U10";
+	if ((age_ < 8) && (age_ >= 6)) category_ = "U8";
+	if ((age_ < 6) && (age_ >= 4)) category_ = "U6";
+	else std::cout << error_message; 
 };
 #endif
--- Roster.h
+++ Roster.h
@@ -13,12 +13,21 @@ class Roster //has the list of players and uses class Player to get info about e
 public:
 	
 	void clear_roster() { roster.clear(); }//to be used for the new season command ---- maybe switch to private eventually?
-	int get_season_year() { return season_year_; }
 
-	int season_year_;
+	void make_player(); //temporary, maybe
 private:
+	int season_year_;
 	Player player_;
 	roster_map_ roster;
 };
 
+void Roster::make_player() {
+	std::string fname;
+	std::string lname;
+	int yob;
+	bool reg_stat;
+	std::cin >> lname >> fname >> yob >> reg_stat;
+	player_(fname, lname, yob, reg_stat, season_year_);
+	if ((player_.age_ >= 6) && (player_.age_ <= 16)) roster[lname] = player_;
+}
 #endif
